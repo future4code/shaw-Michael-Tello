519dd1f478e78b8a3c95b55baa1480b2b8de5c86
Michael Soto <michael_nickolas@hotmail.com>
2022-02-14 14:27:06 -0300

    Variáveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,58 @@
+//1.
+/* 10
+10,5  */
+
+// 2.
+// 20,10,10
+
+// 3.
+/* let hoursDay = prompt("Quantas horas você trabalha por dia?");
+let salaryDay = prompt("Quanto você recebe por dia?");
+alert(`Voce recebe ${hoursDay},${salaryDay} por hora`); */
+
+//Escrita de Codigo
+//1. a e b
+// const namePerson,agePerson
+
+//c.
+/*console.log(`typeof ${namePerson}
+ typeof ${agePerson}`); */
+
+//d.
+// As 2 variaveis não definiram nenhum valor por isso esta undefined
+
+//e.
+/* const namePerson = prompt("Digite seu nome: ");
+const agePerson = prompt("Digite sua idade: ");
+ */
+
+//f.
+/* const namePerson = prompt("Digite seu nome: ");
+const agePerson = prompt("Digite sua idade: "); 
+
+const namePerson = prompt("Digite seu nome: ");
+const agePerson = prompt("Digite sua idade: "); */
+//Variaveis já declaradas não podem ser declaradas novamente com o mesmo nome
+
+//g.
+/* const namePerson = prompt("Digite seu nome: ");
+const agePerson = prompt("Digite sua idade: ");
+console.log(`Ola ${namePerson} você tem ${agePerson} anos`) */
+
+//2. a e b
+/* const question1 = "Você está usando uma roupa azul hoje?";
+const question2 = "Você acordou cedo hoje?";
+const question3 = "Voce saiu de casa hoje";
+console.log(`${question1} - SIM`);
+console.log(`${question2} - NAO`);
+console.log(`${question3} - NAO`); */
+
+let a = 10;
+let b = 25;
+let c = a;
+
+a = b;
+b = c;
+
+console.log("O novo valor de a é", a); // O novo valor de a é 25
+console.log("O novo valor de b é", b); // O novo valor de b é 10
