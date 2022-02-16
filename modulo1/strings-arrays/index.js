af7531f46c10b6ea6c0ede5a34a1d8e4e06670d8
Michael Soto <michael_nickolas@hotmail.com>
2022-02-16 14:22:19 -0300

    Strings e Arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,56 @@
+// ANCHOR Interpretação de código
+
+/*  1.
+a.  undefined
+ b.  undefined
+  c.  11
+	 erro
+	 erro
+	erro
+ */
+
+// 2.  SUBI NUM ÔNIBUS EM MIRROCOS,27
+
+//ANCHOR Escrita de código
+
+// 1.
+/* 		const nomeDoUsuario = prompt("Digite seu nome: ")
+		const emailDoUsuario = prompt("Digite seu e-mail: ")
+console.log(`${emailDoUsuario} foi cadastrado com sucesso. 
+Seja bem-vinda(o),${nomeDoUsuario}  !`) */
+
+// 2.
+// a.
+/* let comidas = ["hamburguer", "pizza", "sushi", "suco", "sorvete"];
+console.log(`${comidas}`); */
+
+// b.
+// console.log("Essas são as minhas comidas preferidas: " , comidas);
+
+// c.
+/* let comidaPreferida = prompt("Qual sua comida favorita:");
+let novaFrase = comidas.splice(comidas.indexOf('pizza'), 1, comidaPreferida);
+console.log(`${comidas}`) */
+
+// 3.
+// a.
+/*  const array = []
+		const listaDeTarefas = array */
+
+// b.
+/*  const tarefa1 = prompt("Digite a 1º tarefa: ")
+		 const tarefa2 = prompt("Digite a 2º tarefa: ")
+		 const tarefa3 = prompt("Digite a 3º tarefa: ")
+		 array.push(tarefa1,tarefa2,tarefa3) */
+
+// c.
+// console.log(array)
+
+// d.
+// const tarefaRealizada = Number(prompt("Digite um número de 0 a 2"))
+
+// e.
+// array.splice(tarefaRealizada, 1)
+
+// f.
+// console.log(array)
