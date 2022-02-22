6262e5912f6d8253a47c38f8f001de3a4abfab0e
michael soto <michael_nickolas@hotmail.com>
2022-02-22 15:32:30 -0300

    Update Desafio

--- modulo1/condicionais/index.js
+++ modulo1/condicionais/index.js
@@ -54,7 +54,7 @@ switch(turno) {
 } */
 
 // 4.
-const pergunta = prompt("qual o gênero de filme que vão assistir? ");
+/* const pergunta = prompt("qual o gênero de filme que vão assistir? ");
 const precoIngresso = Number(prompt("qual o preço do ingresso? "));
 const checarAmigo = prompt("seu amigo topa assistir o filme?");
 switch (checarAmigo) {
@@ -70,6 +70,63 @@ switch (checarAmigo) {
     console.log(
       `Digite novamente,escoha 'sim' ou 'não'  se seu amigo topa assistir o filme `
     );
-}
+} */
 
 //ANCHOR Desafio
+// 1.
+/* const pergunta = prompt("qual o gênero de filme que vão assistir? ");
+const precoIngresso = Number(prompt("qual o preço do ingresso? "));
+const checarAmigo = prompt("seu amigo topa assistir o filme?");
+switch (checarAmigo) {
+  case "sim":
+    const lanche = prompt("qual lanchinho vc vai comprar? (pipoca, chocolate ou doce) ");
+console.log(`Bom filme!  e Aproveite o  ${lanche}`)
+    break;
+  case "nao":
+    console.log(`Escolha outro filme`);
+    break;
+  default:
+    console.log(
+      `Digite novamente,escoha 'sim' ou 'não'  se seu amigo topa assistir o filme `
+    );
+} */
+
+// 2.
+const namePerson = prompt("Digite seu nome: ");
+const gameType = prompt(`Digite o tipo de jogo:
+     IN  -> indica internacional 
+      DO -> indica doméstico;
+     `);
+const gameStage = prompt(`Digite o tipo de jogo:
+     SF -> indica semi-final
+      DT -> indica decisão de terceiro lugar
+        FI -> indica final
+     `);
+const category = prompt(`Digite a categoria: 1, 2, 3 ou 4 ? `);
+const numberTickets = Number(prompt(`Digite a quantida de ingressos: `));
+
+const dolar = 4.1;
+const entryValue = numberTickets * dolar;
+
+console.log(`---Dados da compra---`);
+console.log(`Nome do cliente: ${namePerson} `);
+
+if (gameType === "IN") {
+  console.log(`Tipo do jogo: Internacional`);
+} else if (gameType === "DO") {
+  console.log(`Tipo do jogo: Nacional`);
+}
+
+if (gameStage === "SF") {
+  console.log(`Etapa do jogo: Semifinais`);
+} else if (gameType === " DT") {
+  console.log(`Etapa do jogo: Decisão do 3º lugar`);
+} else if (gameType === "  FI") {
+  console.log(`Etapa do jogo: Final`);
+}
+
+console.log(`Categoria: ${category}`);
+console.log(`Quantidade de ingressos: ${numberTickets} ingressos`);
+console.log(`---Valores--- `);
+console.log(`Valor do ingresso: ${dolar.toFixed(2)}`);
+console.log(`Valor total: ${entryValue.toFixed(2)}`);
