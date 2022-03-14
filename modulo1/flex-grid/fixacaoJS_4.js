3ed91891be4cb1bf353a05cb55c8fae354c934e7
michael soto <michael_nickolas@hotmail.com>
2022-03-14 17:19:08 -0300

    Aula - Flexbox e Grid
    Exercícios de Fixação de Javascript

---
+++ modulo1/flex-grid/fixacaoJS_4.js
@@ -0,0 +1,12 @@
+
+
+ function contaOcorrencias(arrayDeNumeros, numeroEscolhido) {
+  // Escreva seu código aqui
+ const result = arrayDeNumeros.filter((array)=>{
+if (array === 4) {
+  return `O número ${}`
+} else {
+  return ` Número não encontrado`
+}
+ })
+}
