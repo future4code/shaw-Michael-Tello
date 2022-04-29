b38b1361071d53bdbfd32b060fdf01e396a3b248
michael soto <michael_nickolas@hotmail.com>
2022-04-29 16:48:30 -0300

    Identificando  erros na API e material Ui

--- modulo3/projeto-labex/src/components/ApplicationFormPage/index.jsx
+++ modulo3/projeto-labex/src/components/ApplicationFormPage/index.jsx
@@ -8,53 +8,96 @@ import {
   Select,
   TextField,
 } from "@mui/material";
-
-import { headers, labexURL } from "../../constants/labexAPI";
-import useRequestData from "../../hooks/useRequestData";
+import React from "react";
+// import { headers, labexURL } from "../../constants/labexAPI";
+// import useRequestData from "../../hooks/useRequestData";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useForm } from "../../hooks/useForm";
 
 function ApplicationFormPage() {
   const [trips, setTrips] = useState([]);
+  const { form, onChange, cleanFields } = useForm({
+    name: "",
+    age: 0,
+    applicationText: "",
+    profession: "",
+    country: "",
+  });
 
-  // const tripsList = trips.map((trip) => {
-  //   return (
-  //     <MenuItem key={trip.id} value={trip}>
-  //       {trip.name}
-  //     </MenuItem>
-  //   );
-  // });
+  const register = (event) => {
+    event.preventDefault();
+    console.log("Formulário enviado!", form);
+    cleanFields();
+  };
 
-  const getTrip = () => {
-    axios
+ 
+  const getTrip = async () => {
+    await axios
       .get(
-        "https://us-central1-labenu-apis.cloudfunctions.net/labeX/michaelsoto/trips",
-        headers
+        "https://us-central1-labenu-apis.cloudfunctions.net/labeX/michaelsoto/trips"
       )
+
       .then((response) => {
+        console.log(response.data);
         setTrips(response.data.trips);
-        console.log(response);
       })
       .catch((error) => {
-        alert("Erro no Get");
+        alert(error);
       });
   };
 
+  useEffect(() => {
+    getTrip();
+  }, []);
+
+  const tripsList = trips.map((trip) => {
+    console.log(trip);
+    return (
+      <MenuItem key={trip.id} value={trip}>
+        {trip.name}
+      </MenuItem>
+    );
+  });
+
   return (
     <div>
-      <button onClick={getTrip}>Clicar</button>
-      {/* <PageTitle title={"Aplicar para viagem"} />
-      <FormContainer>
-        <TextField label="Nome do candidato" />
-        <TextField label="Idade" type="number" />
+      <PageTitle title={"Aplicar para viagem"} />
+      <FormContainer onSubmit={register}>
         <TextField
-          label="Texto de aplicação"
-          helperText="Explique porque devemos te aprovar?"
+          label={"Nome do candidato"}
+          onChange={onChange}
+          value={form.name}
+          name={"name"}
         />
-        <TextField label="Profissão" />
+        <TextField
+          label={"Idade"}
+          type={"number"}
+          onChange={onChange}
+          value={form.age}
+          name={"age"}
+        />
+        <TextField
+          label={"Texto de aplicação"}
+          helperText="Explique porque devemos te aprovar?"
+          onChange={onChange}
+          value={form.applicationText}
+          name={"applicationText"}
+        />
+        <TextField
+          label={"Profissão"}
+          onChange={onChange}
+          value={form.profession}
+          name={"profession"}
+        />
         <FormControl>
           <InputLabel id="select-paises">Paises</InputLabel>
-          <Select labelId="select-paises">
+          <Select
+            labelId="select-paises"
+            onChange={onChange}
+            value={form.country}
+            name={"country"}
+          >
             <MenuItem value="brasil">Brasil</MenuItem>
             <MenuItem value="argentina">Argentina</MenuItem>
             <MenuItem value="eua">Estados Unidos</MenuItem>
@@ -62,13 +105,19 @@ function ApplicationFormPage() {
         </FormControl>
         <FormControl>
           <InputLabel id="select-viagens">Viagens</InputLabel>
-           <Select labelId="select-viagens">{tripsList}</Select> 
+          <Select
+            labelId="select-viagens"
+            onChange={onChange}
+            value={form.trip}
+            name={"trip"}
+          >
+            {tripsList}
+          </Select>
         </FormControl>
         <Button variant="contained" type="submit">
           Increver-se
         </Button>
-      </FormContainer> 
-    */}
+      </FormContainer>
     </div>
   );
 }
